ea919c256e027fa1204385b31cb693867e34a86f
Daniel O'Neill <oneda538@gmail.com>
2017-01-22 12:57:53 +1300

    changed send_cmd to return all data not just the OK. Also if module is already on correct wifi it doesn't disconnect and reconnect

--- esp8266test.py
+++ esp8266test.py
@@ -22,12 +22,13 @@ def send_cmd(sCmd, waitTm=1, retry=5):
     for i in range(retry):
         ser.flushInput()
         ser.write(sCmd + "\r\n")
-        ret = ser.readline()  # Eat echo of command.
+        ret = []
+        ret.append(ser.readline().strip("\r\r\n"))  # Eat echo of command.
         sleep(0.2)
-        while (lp < waitTm or 'busy' in ret):
+        while (lp < waitTm or Status.BUSY in ret[-1]):
             while (ser.inWaiting()):
-                ret = ser.readline().strip("\r\n")
-                logging.debug(ret)
+                ret.append(ser.readline().strip("\r\n"))
+                logging.debug(ret[-1])
                 lp = 0
             if (ret in Status.OK): break
             # if( ret == 'ready' ): break
@@ -36,7 +37,7 @@ def send_cmd(sCmd, waitTm=1, retry=5):
             lp += 1
 
         sleep(1)
-        if (ret in Status.OK): break
+        if (ret[-1] in Status.OK): break
 
     logging.info("Command result: %s" % ret)
     return ret
@@ -51,8 +52,8 @@ speed = sys.argv[2]
 ssid = sys.argv[3]
 pwd = sys.argv[4]
 
+ser = serial.Serial(port, speed)
 try:
-    ser = serial.Serial(port, speed)
     if ser.isOpen():
         ser.close()
     ser.open()
@@ -61,12 +62,17 @@ try:
     send_cmd("AT")
     # send_cmd( "AT+RST", 5 ) # NOTE: seems to cause problems that require manually reset (pulling down the RST pin)
     # sleep(3)
-    send_cmd("AT+CWMODE=1")  # set device mode (1=client, 2=AP, 3=both)
-    # The mode will be changed on Olimex MOD-WIFI-ESP8266-DEV only after a reset
-    # The command below will reset the device
-    send_cmd("AT+RST");
-    send_cmd("AT+CWLAP", 10)  # scan for WiFi hotspots
-    send_cmd("AT+CWJAP=\"" + ssid + "\",\"" + pwd + "\"", 5)  # connect
+    current_ap = send_cmd("AT+CWJAP?")[1]
+    print current_ap
+    if ssid not in current_ap:
+        send_cmd("AT+CWMODE=1")  # set device mode (1=client, 2=AP, 3=both)
+        # The mode will be changed on Olimex MOD-WIFI-ESP8266-DEV only after a reset
+        # The command below will reset the device
+        send_cmd("AT+RST");
+        send_cmd("AT+CWLAP", 10)  # scan for WiFi hotspots
+        send_cmd("AT+CWJAP=\"" + ssid + "\",\"" + pwd + "\"", 5)  # connect
+
     addr = send_cmd("AT+CIFSR", 5)  # check IP address
+
 finally:
     ser.close()
